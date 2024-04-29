315584282e54adab43ea7e708b268bae15f15ebe
vaibhav-codes <88421446+vaibhav-codes@users.noreply.github.com>
2024-04-29 21:15:56 +0530

    Time: 2 ms (79.80%) | Memory: 53 MB (35.65%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,19 +1,43 @@
 import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map=new HashMap<Integer, Integer>();
-        int ans=Integer.MAX_VALUE;
-        int n=nums.length;
+        //Approach 1
+        // HashMap<Integer, Integer> map=new HashMap<Integer, Integer>();
+        // int ans=Integer.MAX_VALUE;
+        // int n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     if(map.containsKey(nums[i])){
+        //         map.put(nums[i], map.get(nums[i])+1);
+        //     }
+        //     else{
+        //         map.put(nums[i], 1);
+        //     }
+        //     if(map.get(nums[i])>n/2)
+        //     ans=nums[i];
+        // }
+        // return ans;
+        int count=0, element=0, n=nums.length;
         for(int i=0;i<n;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i])+1);
+            if(count==0){
+                element=nums[i];
+                count++;
+            }
+            else if(nums[i]==element){
+                count++;
             }
             else{
-                map.put(nums[i], 1);
+                count--;
             }
-            if(map.get(nums[i])>n/2)
-            ans=nums[i];
         }
-        return ans;
+
+        int counter2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==element)
+            {
+                counter2++;
+            }
+        }
+        if(counter2>n/2) return element;
+        else return -1;
     }
 }
