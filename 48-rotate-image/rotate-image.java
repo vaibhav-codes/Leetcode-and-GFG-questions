d2b6dc0b2c139988dce496aef0616fefccca27a3
vaibhav-codes <88421446+vaibhav-codes@users.noreply.github.com>
2024-06-09 00:43:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (47.15%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,17 +1,43 @@
 class Solution {
     public void rotate(int[][] matrix) {
+        // int n=matrix.length;
+        // int m=matrix[0].length;
+        // int ansMatrix[][]=new int[n][m];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         ansMatrix[i][j]=matrix[m-1-j][i];
+        //     }
+        // }
+
+        //  for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         matrix[i][j]=ansMatrix[i][j];
+        //     }
+        // }
+
+
+        //1st step transpose the matrix i.e col becomes row and row becomes column
         int n=matrix.length;
         int m=matrix[0].length;
-        int ansMatrix[][]=new int[n][m];
+        int temp;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ansMatrix[i][j]=matrix[m-1-j][i];
+            for(int j=i+1;j<m;j++){
+
+                if(i!=j){
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+                }
+                
             }
         }
 
-         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                matrix[i][j]=ansMatrix[i][j];
+        //2nd step is to reverse all the rows of matrix
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m/2;j++){
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[i][m-1-j];
+                matrix[i][m-1-j]=temp;
             }
         }
     }
