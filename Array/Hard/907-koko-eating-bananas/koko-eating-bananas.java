00c5dc14a91570baf5a98d8465e757a1dac84282
vaibhav-codes <88421446+vaibhav-codes@users.noreply.github.com>
2024-10-02 22:14:14 +0530

    Time: 6 ms (99.27%) | Memory: 44.8 MB (89.06%) - LeetSync

--- Array/Hard/907-koko-eating-bananas/koko-eating-bananas.java
+++ Array/Hard/907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,35 +1,31 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;
-        int right = getMaxPile(piles);
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canEatAll(piles, h, mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
+        int left=1;
+        int right=maxPile(piles);
+
+        while(left<right){
+            int mid=(left+right)/2;
+            if(canEatAllBananas(piles,mid,h)){
+                right=mid;
+            }
+            else{
+                left=mid+1;
             }
         }
-        
         return left;
     }
-    
-    private boolean canEatAll(int[] piles, int h, int k) {
-        int hours = 0;
-        for (int pile : piles) {
-            hours += (pile - 1) / k + 1;
-            if (hours > h) {
-                return false;
-            }
+    public boolean canEatAllBananas(int piles[], int k, int h){
+        int hours=0;
+        for(int pile:piles){
+            hours+=(pile-1)/k + 1;
+            if(hours>h) return false;
         }
         return true;
     }
-    
-    private int getMaxPile(int[] piles) {
-        int max = 0;
-        for (int pile : piles) {
-            max = Math.max(max, pile);
+    public int maxPile(int piles[]){
+        int max=0;
+        for(int pile: piles){
+            max=Math.max(max, pile);
         }
         return max;
     }
